4ee321b784f38d6e622180e3308ddcd192fc4ad9
ubuntu <William Wolff>
2022-03-22 15:26:11 +0000

    [DOCKER] Updated docker run script for additional comments and override existing container

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-docker run -it --name nft \
+# Replace relay1-ipc with container volume for node socket
+# Alternatively, run cardano-node container, let it sync.
+# docker run -it --rm \
+#  	-v node-config:/opt/cardano/config \
+#  	-v node-data:/opt/cardano/data \
+#  	-v node-ipc:/opt/cardano/ipc \
+#	nessusio/cardano-node
+docker run -it --name nft --rm \
 	-v relay1-ipc:/opt/cardano/ipc \
 	-v $(pwd)/inputs:/var/cardano/inputs \
 	psg/nft:latest
