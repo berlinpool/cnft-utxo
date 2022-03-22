8a81341f163f85fae1994c0caced64e41453eda3
ubuntu <William Wolff>
2022-03-22 12:20:03 +0000

    [DOCKER] Added docker start script to run container multiple times

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -it --name nft \
-	-v node-ipc:/opt/cardano/ipc \
+	-v relay1-ipc:/opt/cardano/ipc \
 	-v $(pwd)/inputs:/var/cardano/inputs \
 	psg/nft:latest
---
+++ docker-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Use this script to run an existing container named nft multiple times
+# Remeber to update the inputs - in particular the utxo file
+docker start -i nft
