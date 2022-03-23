16743fd230132ff6b7dc18d1e2852a8ce5d62390
ubuntu <William Wolff>
2022-03-23 09:09:53 +0000

    [DOCKER] Added support for running on testnet

--- docker-run.sh
+++ docker-run.sh
@@ -6,7 +6,26 @@
 #  	-v node-data:/opt/cardano/data \
 #  	-v node-ipc:/opt/cardano/ipc \
 #	nessusio/cardano-node
-docker run -it --name nft --rm \
-	-v relay1-ipc:/opt/cardano/ipc \
-	-v $(pwd)/inputs:/var/cardano/inputs \
-	psg/nft:latest
+
+if [ -z "$1" ]; then
+	echo "No network supplied (mainnet|testnet)"
+    exit 1
+  else 
+    NETWORK=$1
+fi
+
+
+if [[ $NETWORK == "mainnet" ]]; then
+	docker run -it --name nft --rm \
+		-v relay1-ipc:/opt/cardano/ipc \
+		-v $(pwd)/inputs:/var/cardano/inputs \
+		-e NETWORK=$NETWORK \
+		psg/nft:latest
+else
+	docker run -it --name nft-test --rm \
+		-v test-relay-ipc:/opt/cardano/ipc \
+		-v $(pwd)/inputs:/var/cardano/inputs \
+		-e NETWORK=$NETWORK \
+		psg/nft:latest
+fi
+
