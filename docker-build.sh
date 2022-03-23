faa0c02df348e9266f4ecb39850a88c87cb02800
ubuntu <William Wolff>
2022-03-23 09:20:04 +0000

    [DOCKER] added default version for docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker build --label psg/nft -t psg/nft:$1 -t psg/nft:latest .
+
+if [ -z "$1" ]; then
+    echo "No image version supplied (default: 1.0.0)"
+	VERSION=1.0.0
+  else 
+    VERSION=$1
+fi
+
+docker build --label psg/nft -t psg/nft:$VERSION -t psg/nft:latest .
