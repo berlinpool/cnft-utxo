aed546c18b658ac53d51317f8092c9731582f88e
William Wolff <william.wolff@iohk.io>
2022-04-07 09:54:56 +0000

    [SCRIPTS] Simplified copying of dynamically linked libraries from nix store to tmp nix store

--- scripts/build/build-nft-binaries-store.sh
+++ scripts/build/build-nft-binaries-store.sh
@@ -10,45 +10,23 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command failed with exit code $?"' EXIT
 
 storePath=/tmp/nix-store/
-echo "Creating temporary nix store " $storePath
+#echo "Creating temporary nix store " $storePath
 mkdir -p $storePath
 
-cd /tmp/plutus-apps/
-nix-shell --run 'cd /tmp/nft/ && cabal update && cabal install && cp $(which cardano-cli) /usr/local/bin/cardano-cli 2>&1' 
-
-binaries=("cardano-cli" "token-name" "token-policy")
-for binary in $binaries; do
-    echo "Finding $binary runtime dependencies"
-    binPath=$(which "$binary")
-    binDeps=$(ldd $binPath | tr -s ' ' | cut -d ' ' -f 3)
-
-    echo "$binary: Copying $(echo $binDeps | wc -w | awk '{print int($1/2)}') runtime binaries to $storePath"
-    for dep in $binDeps; do
-        echo $dep
-        depDir=$(dirname $dep)
-        destPath=$storePath$depDir
-        mkdir -p $destPath
-        cp $dep $destPath
-    done
-done
-
-binaries=("cardano-cli" "token-name" "token-policy")
-for binary in $binaries; do
-    echo "Finding $binary runtime dependencies"
-    binPath=$(which "$binary")
-    binDeps=$(ldd $binPath | tr -s ' ' | cut -d ' ' -f 1)
-
-    echo "$binary: Copying $(echo $binDeps | wc -w | awk '{print int($1/2)}') runtime binaries to $storePath"
-    echo $binDeps
-    for dep in $binDeps; do
-        echo "Checking: "
-        echo $dep
-        if [[ $dep == "/nix/store/*" ]]; then
-            echo "Found nix dependencies: $dep"
-            depDir=$(dirname $dep)
-            destPath=$storePath$depDir
-            mkdir -p $destPath
-            cp $dep $destPath
-        fi
+declare -a binaries=("token-name" "token-policy" "cardano-cli")
+for binary in "${binaries[@]}"
+do
+    echo "Finding $binary runtime dependencies:"
+    binPath="$(which $binary)"
+    # filter for all dynamically linked /nix/store dependencies of binary
+    binDeps=$(ldd $binPath | cut -d ' ' -f 1,3 | xargs | tr ' ' '\n' | grep '^/nix/store/')
+    echo "$binary: Copying $(echo $binDeps | wc -w) runtime binaries to $storePath"
+    for dep in $binDeps
+    do
+        echo ""
+        depPath=$(dirname $dep)
+        mkdir -p $storePath$depPath
+        echo "Copying $dep to $storePath$depPath"
+        cp $dep "$storePath$depPath/$(basename $dep)" || true
     done
 done
